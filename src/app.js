26da89bab9c77593b3b00f7ff9f95cef9cb83c49
Karthik Shasthri <karthikshasthri@Karthiks-MacBook-Air.local>
2022-06-03 14:05:42 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,90 +1,95 @@
 //Libraries
-const express = require('express');
-const path = require('path');
-const hbs = require('hbs');
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
 
 //Functions
-const geocode = require('../src/utils/geocode');
-const forecast = require('../src/utils/forecast');
+const geocode = require("../src/utils/geocode");
+const forecast = require("../src/utils/forecast");
 
 const app = express();
 
-// Define paths for Express config 
-const publicDirPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
-const partialPath = path.join(__dirname, '../templates/partials');
+const port = process.env.PORT || 3000;
+
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 hbs.registerPartials(partialPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirPath));
 
 //Tab routers
-app.get('', (req, res) => {
-    res.render('index',{
-            title: 'Weather',
-            name: '© Karthik Shasthri'
-        });
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "© Karthik Shasthri",
+  });
 });
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About',
-        name: '© Karthik Shasthri'
-    });
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "© Karthik Shasthri",
+  });
 });
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        name: '© Karthik Shasthri',
-        title: 'Help'
-    });
+app.get("/help", (req, res) => {
+  res.render("help", {
+    name: "© Karthik Shasthri",
+    title: "Help",
+  });
 });
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({ error:'Please enter the location'});
-    }
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({ error: "Please enter the location" });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(longitude, latitude, (error, result) => {
         if (error) {
-            return res.send({ error });
+          return res.send({ error });
         }
-        forecast(longitude, latitude, (error, result) => {
-            if (error) {
-                return res.send({ error });
-            }
-            res.send({
-                forecast: result,
-                location,
-                address:req.query.address
-            });
+        res.send({
+          forecast: result,
+          location,
+          address: req.query.address,
         });
-    });
+      });
+    }
+  );
 });
 
 //Error router
-app.get('/help/*', (req, res) => {
-    res.render('404Error', {
-        title: '404',
-        name: 'Karthik Shastri',
-        errorMessage:'Help Article not found'
-    });
+app.get("/help/*", (req, res) => {
+  res.render("404Error", {
+    title: "404",
+    name: "Karthik Shastri",
+    errorMessage: "Help Article not found",
+  });
 });
 
 // 404 Not Found Error
-app.get('*', (req, res) => {
-    res.render('404Error', {
-        title: '404',
-        name: 'Karthik Shastri',
-        errorMessage:'Page not found'
-    });
+app.get("*", (req, res) => {
+  res.render("404Error", {
+    title: "404",
+    name: "Karthik Shastri",
+    errorMessage: "Page not found",
+  });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
 
 // Old code
